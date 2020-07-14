f5818346d156bd06e7e0a42528fcc6caa19c7fa7
Lucas MOULIN <lmoulin@e1r10p23.42.fr>
2020-07-14 18:30:40 +0200

    minishell

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 15:37:39 by lmoulin           #+#    #+#             */
-/*   Updated: 2020/07/14 17:42:11 by lmoulin          ###   ########.fr       */
+/*   Updated: 2020/07/14 18:30:05 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,36 @@
 #include "../libft/libft/libft.h"
 #include "../includes/color.h"
 #include "unistd.h"
+#include "stdio.h"
 # define BUF_SIZE 2048
+
+void		ft_cd(char **buf)
+{
+
+}
 
 int main(int ac, char **av, char **env)
 {
 	int		ret;
+	int		i;
+	int		pos;
 	char	buf[BUF_SIZE + 1];
 	char	*dir;
 
 	while (ft_strncmp(buf, "exit", ft_strlen("exit")))
 	{
 		dir = getcwd(dir, BUF_SIZE);
-		ft_printf(1, "" BOLDGREEN "➜ " RESET BOLDCYAN " %s " RESET, dir);
+		i = ft_strlen(dir);
+		while (i >= 0 && dir[i] != '/')
+			i--;
+		ft_printf(1, "" BOLDGREEN "➜ " RESET BOLDCYAN " %s " RESET, &dir[i + 1]);
 		ret = read(0, buf, BUF_SIZE);
+		buf[ret] = '\0';
+		if (!ft_strncmp(buf, "cd ", ft_strlen("cd ")))
+		{
+			chdir("..");
+			printf("%s| chdir\n", buf + 3);
+		}
 	}
 
 }
