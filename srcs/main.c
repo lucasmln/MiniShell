3313580e640741fbf5af26d44edd0c7551db8002
Lucas MOULIN <lmoulin@e1r10p23.42.fr>
2020-07-14 17:43:54 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,29 +6,27 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 15:37:39 by lmoulin           #+#    #+#             */
-/*   Updated: 2020/07/14 17:33:54 by lmoulin          ###   ########.fr       */
+/*   Updated: 2020/07/14 17:42:11 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include "../libft/libft/libft.h"
-
+#include "../includes/color.h"
 #include "unistd.h"
 # define BUF_SIZE 2048
 
-#define BOLDRED     "\033[1m\033[31m"
-#define BOLDCYAN    "\033[1m\033[36m"
-#define RESET		"\033[0m"
-
 int main(int ac, char **av, char **env)
 {
-	int		i;
+	int		ret;
 	char	buf[BUF_SIZE + 1];
 	char	*dir;
 
-	while (ft_strncmp(buf, "exit", ft_strlen(buf)))
+	while (ft_strncmp(buf, "exit", ft_strlen("exit")))
 	{
 		dir = getcwd(dir, BUF_SIZE);
-		ft_printf(1, "" BOLDRED "➜ " RESET BOLDCYAN " %s " RESET, dir);
+		ft_printf(1, "" BOLDGREEN "➜ " RESET BOLDCYAN " %s " RESET, dir);
+		ret = read(0, buf, BUF_SIZE);
 	}
+
 }
