8726a5d58bb6d2f0215a8b6b0e71823cf1d387a4
Lucas MOULIN <lmoulin@e1r10p23.42.fr>
2020-07-14 19:02:44 +0200

    minishell

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,33 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 15:37:39 by lmoulin           #+#    #+#             */
-/*   Updated: 2020/07/14 18:30:05 by lmoulin          ###   ########.fr       */
+/*   Updated: 2020/07/14 19:02:32 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 #include "../libft/libft/libft.h"
 #include "../includes/color.h"
-#include "unistd.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "stdio.h"
 # define BUF_SIZE 2048
 
-void		ft_cd(char **buf)
+void		ft_cd(char *buf)
 {
+	struct stat	info;
+	int			i;
 
+	i = 2;
+	while (buf[i] && buf[i] == ' ')
+		i++;
+	ft_strlcat(&buf[i], "c:\\", ft_strlen(&buf[i]));
+	printf("stat return = %d\n", stat(buf, &info));
+	if (stat(buf, &info))
+		ft_printf(1, "File not found.");
+	else
+		chdir(&buf[i]);
 }
 
 int main(int ac, char **av, char **env)
@@ -41,7 +54,8 @@ int main(int ac, char **av, char **env)
 		buf[ret] = '\0';
 		if (!ft_strncmp(buf, "cd ", ft_strlen("cd ")))
 		{
-			chdir("..");
+			ft_cd(buf);
+//			chdir("..");
 			printf("%s| chdir\n", buf + 3);
 		}
 	}
