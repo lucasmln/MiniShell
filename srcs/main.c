ca0c6afef6055454e103e1154a35ad4940ca8fb1
Lucas MOULIN <lmoulin@e1r2p22.42.fr>
2020-07-15 13:52:10 +0200

    cd

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 15:37:39 by lmoulin           #+#    #+#             */
-/*   Updated: 2020/07/14 19:02:32 by lmoulin          ###   ########.fr       */
+/*   Updated: 2020/07/15 13:51:57 by lmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,39 @@
 #include "stdio.h"
 # define BUF_SIZE 2048
 
-void		ft_cd(char *buf)
+void		ft_error_cd(struct stat info, char *buf)
+{
+	if (S_ISREG(info.st_mode))
+		ft_printf(1, "cd: not a directory: %s\n", buf);
+	else
+		ft_printf(1, "cd: no such file or directory: %s\n", buf);
+}
+
+void		ft_cd(char *buf, char *dir)
 {
 	struct stat	info;
 	int			i;
+	int			o;
+	char		*new_dir;
 
-	i = 2;
+	i = 0;
+	while (buf[i] && buf[i] != '\n')
+		i++;
+	buf[i] = '\0';
+	i = 0;
 	while (buf[i] && buf[i] == ' ')
 		i++;
-	ft_strlcat(&buf[i], "c:\\", ft_strlen(&buf[i]));
-	printf("stat return = %d\n", stat(buf, &info));
-	if (stat(buf, &info))
-		ft_printf(1, "File not found.");
-	else
-		chdir(&buf[i]);
+	if (stat(&buf[i], &info) == -1 || S_ISREG(info.st_mode))
+	{
+		ft_error_cd(info, &buf[i]);
+		return;
+	}
+	chdir(&buf[i]);
+}
+
+void		ft_echo(char *buf)
+{
+
 }
 
 int main(int ac, char **av, char **env)
@@ -52,12 +71,29 @@ int main(int ac, char **av, char **env)
 		ft_printf(1, "" BOLDGREEN "➜ " RESET BOLDCYAN " %s " RESET, &dir[i + 1]);
 		ret = read(0, buf, BUF_SIZE);
 		buf[ret] = '\0';
-		if (!ft_strncmp(buf, "cd ", ft_strlen("cd ")))
-		{
-			ft_cd(buf);
-//			chdir("..");
-			printf("%s| chdir\n", buf + 3);
-		}
+		i = 0;
+		while (buf[i] && buf[i] == ' ')
+			i++;
+		if (!ft_strncmp(&buf[i], "cd ", ft_strlen("cd ")))
+			ft_cd(&buf[i + 3], dir);
+		if (!ft_strncmp(&buf[i], "echo ", ft_strlen("echo ")))
+			ft_echo(&buf[i]);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
